2a5d50078a4fb0805c866f7108fb636248bd8d19
qiaoster <zimuzhang1@gmail.com>
2024-05-12 10:08:02 +0800

    esese

--- main.c
+++ main.c
@@ -7,14 +7,27 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-//#define NK_IMPLEMENTATION
-//#include <nuklear.h>
+/*
+#define NK_IMPLEMENTATION
+#define NK_PRIVATE
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_INCLUDE_FONT_BAKING
+#include <nuklear.h>
+*/
 
-//#define STB_TRUETYPE_IMPLEMENTATION
-//#include <stb/stb_truetype.h>
+#define STB_RECT_PACK_IMPLEMENTATION
+#include <stb/stb_rect_pack.h>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#define STBTT_STATIC
+#include <stb/stb_truetype.h>
 
 #include "utils.h"
 #include "qiao_math.c"
+
+int SCREEN_WIDTH = 1024;
+int SCREEN_HEIGHT = 768;
 
 GLuint LoadShaders(const char * vertexShaderPath,const char * fragmentShaderPath) {
     /* Create the shaders */
@@ -150,6 +163,32 @@ error_callback(int error, const char* description) {
     printf("Error %d: %s\n", error, description);
 }
 
+/*
+float
+NK_TextWidth(nk_handle handle, float height, const char* text, int len) {
+    //TODO
+    return 100;
+}
+*/
+
+void
+DrawText(float x, float y, char* text, GLuint tex, stbtt_bakedchar* cdata) {
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glBegin(GL_QUADS);
+    while (*text) {
+        if (*text >= 32 && (int)*text < 128) {
+            stbtt_aligned_quad quad;
+            stbtt_GetBakedQuad(cdata, 1024, 1024, *text - 32, &x, &y, &quad, 1);
+            glTexCoord2f(quad.s0, quad.t0); glVertex2f(quad.x0, quad.y0);
+            glTexCoord2f(quad.s1, quad.t0); glVertex2f(quad.x1, quad.y0);
+            glTexCoord2f(quad.s1, quad.t1); glVertex2f(quad.x1, quad.y1);
+            glTexCoord2f(quad.s0, quad.t1); glVertex2f(quad.x0, quad.y1);
+        }
+        ++text;
+    }
+    glEnd();
+}
+
 int
 main() {
     bool running = true;
@@ -171,9 +210,11 @@ main() {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_TRUE);
+        glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        window = glfwCreateWindow(1080,720, "dev", NULL, NULL);
+        window = glfwCreateWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "dev", NULL, NULL);
         if (window == NULL) {
             printf("Failed to open GLFW window\n");
             glfwTerminate();
@@ -230,9 +271,12 @@ main() {
         }
     }
 
-    /*
+    /* stb ttf init */
     unsigned char* ttfBuffer = (unsigned char*)malloc(sizeof(unsigned char) * 1<<25);
-    stbtt_fontinfo font;
+    unsigned char* tempBitmap = (unsigned char*)malloc(sizeof(unsigned char) * 1024*1024);
+    stbtt_bakedchar cdata[96];
+    GLuint tex;
+
     //struct nk_user_font nkFont;
     if (running) {
         printf("Loading font...");
@@ -243,27 +287,44 @@ main() {
             printf("Failed to load %s\n", ttfFileName);
         } else {
             fread(ttfBuffer, 1, 1<<25,ttfFile);
-            stbtt_InitFont(&font, ttfBuffer, stbtt_GetFontOffsetForIndex(ttfBuffer, 0));
+            stbtt_BakeFontBitmap(ttfBuffer, 0, 64.0, tempBitmap, 1024, 1024, 32, 96, cdata);
+            glGenTextures(1, &tex);
+            glBindTexture(GL_TEXTURE_2D, tex);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, 1024, 1024, 0, GL_ALPHA, GL_UNSIGNED_BYTE, tempBitmap);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
             fclose(ttfFile);
             printf("Successful.\n");
         }
     }
-    */
 
     /*
     struct nk_context nkContext;
     struct nk_user_font nkFont;
-    nk_init_fixed(&nkContext, calloc(1,4096), 4096, &nkFont);
-*/
+    struct nk_font_atlas atlas;
+    nk_font_atlas_init_default(&atlas);
+    nk_font_atlas_begin(&atlas);
+    struct nk_font* font2 = nk_font_atlas_add_from_memory(&atlas, &ttfBuffer, 16, 0, NULL);
+    int img_width, img_height = 0;
+    const void* img = nk_font_atlas_bake(&atlas, &img_width, &img_height, NK_FONT_ATLAS_RGBA32);
+    nk_font_atlas_end(&atlas, nk_handle_id(texture), 0);
+    nkFont.userdata.ptr = &ttfBuffer;
+    nkFont.height = 10;
+    nkFont.width = NK_TextWidth;
+
+    nk_init_default(&nkContext, &nkFont);
+    */
 
     if (running) {
         glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
         glfwPollEvents();
     }
 
     printf("\nInit done, enter main loop\n");
-    while (running && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
+    while (running &&
+           glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0) {
+        /*
         for (int i = 0; i < vertexCount; ++i) {
             vertexData[i] = Vec4_Transform(x10, vertexData[i]);
             vertexData[i] = Vec4_Rotate(vertexData[i], 'z', Rad(1.0f));
@@ -274,7 +335,11 @@ main() {
 
         glBufferData(GL_ARRAY_BUFFER, sizeof(flatVertexData), flatVertexData, GL_STATIC_DRAW);
 
+        glViewport(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDisable(GL_CULL_FACE);
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_BLEND);
         glUseProgram(programID);
 
         glEnableVertexAttribArray(0);
@@ -289,10 +354,52 @@ main() {
         );
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glDisableVertexAttribArray(0);
+        */
 
+        glClearColor(0.45f, 0.45f, 0.75f, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, tex);
+
+        glColor3f(1,0,0);
+
+        const char* text = "This is a simple test!";
+        //DrawText(100,150,"This is a simple test!", tex, cdata);
+
+        glBegin(GL_QUADS);
+        float x, y = 0;
+        while (*text) {
+            if (*text >= 32 && (int)*text < 128) {
+                stbtt_aligned_quad q;
+                stbtt_GetBakedQuad(cdata, 512, 512, *text - 32, &x, &y, &q, 1);
+/*
+ */
+                glTexCoord2f(q.s0,q.t0); glVertex2f(q.x0,q.y0);
+                glTexCoord2f(q.s1,q.t0); glVertex2f(q.x1,q.y0);
+                glTexCoord2f(q.s1,q.t1); glVertex2f(q.x1,q.y1);
+                glTexCoord2f(q.s0,q.t1); glVertex2f(q.x0,q.y1);
+
+            }
+            ++text;
+        }
+        glEnd();
+
+/*
+        glBegin(GL_QUADS);
+            glTexCoord2f(0,0); glVertex2f(256,200+0);
+            glTexCoord2f(1,0); glVertex2f(768,200+0);
+            glTexCoord2f(1,1); glVertex2f(768,200+512);
+            glTexCoord2f(0,1); glVertex2f(256,200+512);
+        glEnd();
+*/
         /* nk section
         if (nk_begin(&nkContext, "Show", nk_rect(50,50,220,220), NK_WINDOW_BORDER)) {
+            if (nk_button_label(&nkContext, "button")) {
 
+            }
         } 
         nk_end(&nkContext);
         */
@@ -301,7 +408,8 @@ main() {
         glfwPollEvents();
     }
 
-    //free(ttfBuffer);
+    free(ttfBuffer);
+    free(tempBitmap);
     glfwTerminate();
     return 0;
 }
--- make
+++ make
@@ -2,6 +2,6 @@
 
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Building at $current_time"
-zig cc -Wall -g -o exe main.c -lGL -lGLU -lGLEW -lglfw &&
-#gcc -Wall -g -o exe main.c -lGL -lGLU -lGLEW -lglfw &&
+zig cc -Wall -g -o exe main.c -lm -lGL -lGLU -lGLEW -lglfw &&
+#gcc -Wall -g -o exe main.c -lm -lGL -lGLU -lGLEW -lglfw &&
 echo "\nBuild Complete.\nRunning:\n" && ./exe && echo "\nFinished.\n"
